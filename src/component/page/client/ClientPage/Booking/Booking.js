ead81435365a9d17b0c4accebf3d3d6ed785e025
lekhoe <lekhoeth@gmail.com>
2022-06-06 23:27:15 +0700

    khoe: banner booking

--- src/component/page/client/ClientPage/Booking/Booking.js
+++ src/component/page/client/ClientPage/Booking/Booking.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import "./booking.scss";
-import {DatePicker, ConfigProvider, Checkbox,} from 'antd';
+import { DatePicker, ConfigProvider, Checkbox, } from 'antd';
 import viVN from 'antd/lib/locale/vi_VN';
 import banner1 from '../../../../../image/banner1.jpg';
 
 
 import 'antd/dist/antd.css';
-import  moment  from "moment";
+import moment from "moment";
 Booking.propTypes = {
 
 };
@@ -19,27 +19,36 @@ Booking.defaultProps = {
 function Booking(props) {
 
     const [time, setTime] = useState(moment(new Date()))
-    let minDate = new  Date ( )
+    let minDate = new Date()
 
     console.log(time)
-    return(
+    return (
         <div className="booking-container">
-            <div className="booking-container__banner" 
-                style={{backgroundImage: `url(${banner1})`}}
+            <div className="booking-container__banner"
+                style={{ backgroundImage: `url(${banner1})` }}
             >
             </div>
-            <div className="booking-container__booke" 
-                style={{backgroundImage: `url(${banner1})`}}
-            >
+            <div className="booking-container__book">
+                <div className="booking-container__book-left">
+                    <div className="booking-container__book-left-label">
+                        SET TABLE
+                    </div>
+                    <div className="booking-container__book-left-content">
+                        SET TABLE
+                    </div>
+                </div>
+                <div className="booking-container__book-right">
+
+                </div>
             </div>
             <ConfigProvider locale={viVN} >
-            <DatePicker onChange={(val)=>{setTime(val)}} defaultValue={time} format={"DD-MM-yyyy"}/>
+                <DatePicker onChange={(val) => { setTime(val) }} defaultValue={time} format={"DD-MM-yyyy"} />
             </ConfigProvider>
-{/* 
+            {/* 
             <DateTimePicker disableClock={true} onChange={setTime} value={time} format={"dd-MM-yyyy"} minDate={minDate}/> */}
 
-            <Checkbox/>
-           
+            <Checkbox />
+
         </div>
     )
 }
