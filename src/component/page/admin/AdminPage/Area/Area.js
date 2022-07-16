ef0a87771463652203877f1f1fa7bdc6f203fef6
lekhoe <lekhoeth@gmail.com>
2022-07-16 19:26:46 +0700

    khoe: khu vực

--- src/component/page/admin/AdminPage/Area/Area.js
+++ src/component/page/admin/AdminPage/Area/Area.js
@@ -5,38 +5,43 @@ import AdminPage from "../AdminPage";
 import { Draggable } from 'drag-react';
 import Button2 from "../../../../base/Button/Button";
 import baseApi from "../../../../../api/baseApi"
+import $ from 'jquery';
+import "./area.scss"
 
 function Area(props) {
 
 
-    
+
 
     const [list, setList] = useState([
         {
-            title: "hahaaa", 
-            top: 500,
-            left: 600
+            title: "hahaaa",
+            top: 0,
+            left: 0
         },
 
         {
             title: "hihiiii",
-            top: 400,
-            left: 300
+            top: 0,
+            left: 0
         },
 
         {
             title: "hohoooo",
-            top: 100,
-            left: 800
+            top: 0,
+            left: 0
         },
     ])
 
     function updatePosition(item, value) {
+        // debugger
+        let height = $(window).height();
+        let width = $(window).width();
         let abc = list
         let objIndex = abc.findIndex((obj => obj.title == item.title));
 
-        abc[objIndex].top = value?.top
-        abc[objIndex].left = value?.left
+        abc[objIndex].top = parseFloat(((value?.top/height)*100).toFixed(2))
+        abc[objIndex].left = parseFloat(((value?.left/width)*100).toFixed(2)) 
 
         setList(abc)
     }
@@ -44,36 +49,26 @@ function Area(props) {
     function lur() {
         console.log(list)
     }
-    
+
 
     return (
         <AdminPage
             title={"Quản lý khu vực"}
             index={MENU_TAB_ADMIN.AREA}
         >
-            <div style={{ backgroundColor: 'red' }}>dsdfsdfsadfasd</div>
-            {/* <Draggable
-                // axis="x"
-                // handle=".handle"
-                // defaultPosition={{ x: 0, y: 0 }}
-                // position={null}
-                // grid={[25, 25]}
-                // scale={1}
-            >
-                <div>
-                    <div className="handle">Khỏe</div>
-                </div>
-                
-            </Draggable> */}
+            <div className="area-manager">
+                {list.map((item) => {
+                    return (
+                        <Draggable onDragEnd={(val, val2) => { updatePosition(item, val) }} key={1} style={{ top: `${item.top}%`, left: `${item.left}%` }} className={item?.title}>
+                            <div style={{ border: '1px solid #000', borderRadius: '8px', width: '100px' }} className="table-item">{item?.title}</div>
+                        </Draggable>
+                    )
+                })}
 
-            {list.map((item) => {
-                return (
-                    <Draggable onDragEnd={(val, val2)=>{updatePosition(item, val)}} key={1} style={{top: item.top, left: item.left}} className={item?.title}>
-                        <div style={{border: '1px solid #000', borderRadius: '8px', width: '100px'}} className="table-item">{item?.title}</div>
-                    </Draggable>
-                )
-            })}
-<Button2 name={"lưu"} onClick={()=>{lur()}}/>
+
+
+                <Button2 name={"lưu"} onClick={() => { lur() }} />
+            </div>
 
         </AdminPage>
 
